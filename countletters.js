02aad7415fcbd57e00f6df3cfd05da2380694fe3
bborncr <bentley@crcibernetica.com>
2017-06-28 15:49:08 +0000

    got the looping working!

--- countletters.js
+++ countletters.js
@@ -1,12 +1,24 @@
 function countLetters(inputText){
+
   // remove spaces
   var inputText = inputText.split(' ').join('');
-  //split the inputText into an array
-  var inputText = inputText.split('');
-  //create the letterCount object
 
-  return inputText;
-  //return letterCount;
+  // split the inputText into an array
+  var inputText = inputText.split('');
+
+  // create the letterCount object
+  var letterCount = {};
+
+  // loop through and count the letters
+  for (var i = 0; i < inputText.length; i++) {
+    if (letterCount[inputText[i]] !== undefined){
+      letterCount[inputText[i]] = letterCount[inputText[i]] + 1;
+    } else {
+      letterCount[inputText[i]] = 1;
+    }
+  }
+
+  return letterCount;
 }
 
 console.log(countLetters("This is a test"));
